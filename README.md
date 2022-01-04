bfad554993dc231422e53f9dbd98db85435a7168
Himalaya <yugmani@yahoo.com>
2022-01-04 15:22:26 -0800

    clipboard function

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # generate_hexa_color
+
+### Generate Hex Color Code
+
+- A user can generate hex color code.
+- A user can see the hex color as background color of application.
+- A user can copy the hex color code into clipboard and use the code somewhere else.
+
+### Technology Used
+
+- HTML
+- CSS
+- JavaScript
+- fontawesome
--- script.js
+++ script.js
@@ -1,9 +1,35 @@
 const hexEl = document.getElementById("hex");
 const btnEl = document.querySelector(".colorBtn");
 const bodyEl = document.querySelector("body");
+const clipEl = document.getElementById("clipboard");
 
+// Array of Hexadecimal digits
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
 
+// To change styles based on the hex color code
+const applyHexCode = () => {
+  let hexCode = generateCode();
+
+  if (hexCode === "#000000") {
+    btnEl.style.backgroundColor = "#FFF";
+    btnEl.style.color = "#000";
+    hexEl.style.color = "#FFF";
+    clipEl.style.color = "#0000FF";
+  } else if (hexCode === "#FFFFFF") {
+    btnEl.style.backgroundColor = "#000";
+    hexEl.style.color = "#000";
+    clipEl.style.color = "#0000FF";
+  } else {
+    btnEl.style.backgroundColor = "#000";
+    hexEl.style.color = "#FFF";
+    clipEl.style.color = "#FFF";
+  }
+
+  bodyEl.style.backgroundColor = hexCode;
+  hexEl.innerText = hexCode;
+};
+
+// to generate hex color code
 const generateCode = () => {
   let hexCode = `#`;
 
@@ -12,8 +38,19 @@ const generateCode = () => {
     hexCode += hex[random];
   }
 
-  bodyEl.style.backgroundColor = hexCode;
-  hexEl.innerText = hexCode;
+  return hexCode;
 };
 
-btnEl.addEventListener("click", generateCode);
+// function to copy hex color code into clipboard
+const copyToClipboard = (text) => {
+  navigator.clipboard.writeText(text);
+};
+
+// Event listener on button click
+btnEl.addEventListener("click", applyHexCode);
+
+// Event listener on clipboard click
+clipEl.addEventListener("click", () => {
+  const textClip = hexEl.textContent;
+  copyToClipboard(textClip);
+});
